1c74fc79b56d6c8f5637792fe102f1f584244878
Gaetano <gaetano.mondelli@gmail.com>
2022-01-16 01:03:37 +0000

    fix: linting + set  solidity.packageDefaultDependenciesDirectory to packages/hardhat/node_modules for fixining not found error

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -99,7 +99,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
+        version: "0.8.2",
         settings: {
           optimizer: {
             enabled: true,
@@ -107,15 +107,6 @@ module.exports = {
           }
         }
       },
-      {
-        version: "0.6.7",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      }
     ],
 
   },
